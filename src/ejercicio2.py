2e1165dcb83afa1eebc747d0ca52fb94250b9c3a
Marina-Mendoza02 <marinams@lcg.unam.mx>
2025-08-13 19:19:03 -0600

    ejercicio 2: Creación de clase

---
+++ src/ejercicio2.py
@@ -0,0 +1,26 @@
+class Gen:
+    
+    def __init__(self, nombre, secuencia, especie=None):
+        """Constructor y atributos"""
+        self.nombre = nombre
+        self.secuencia = secuencia
+        self.especie = especie
+        
+        bases_validas = {'A', 'T', 'G', 'C'}
+        if not all(base in bases_validas for base in self.secuencia):
+            raise ValueError("La secuencia contiene caracteres no validos (solo A, T, G, C permitidos).")
+        
+    
+    def longitud(self):
+        """Regresa la longitud de la secuencia en bases"""
+        return len(self.secuencia)
+    
+    def resumen(self):
+        """Devuelve un resumen breve con la info del gen"""
+        return f"Gen: {self.nombre} ({self.especie if self.especie else 'Especie desconocida'}), {self.longitud()} bases."
+    
+
+# Ejemplo
+gen1 = Gen("lacZ", "ATGACCATGATTACGCCAAGCTT", "Escherichia coli")
+print(gen1.longitud())
+print(gen1.resumen())
